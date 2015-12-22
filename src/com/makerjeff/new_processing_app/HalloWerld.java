e50aff55dd984ed9ec663fe61a91200b8e1161d8
Jefferson Wu <makerjeffwu@gmail.com>
2015-12-21 17:03:32 -0800

    Processing app is reading serial info from arduino

--- src/com/makerjeff/new_processing_app/HalloWerld.java
+++ src/com/makerjeff/new_processing_app/HalloWerld.java
@@ -8,40 +8,77 @@ public class HalloWerld extends PApplet {
 	
 	//global vars ==========
 	public Serial myPort;
+	float xPos = 0.0f;
+	float yPos = 0.0f;
+	float lineWidth = 2f;
+	float refresh = 30f;
+	
+	int appWidth = 1920;
+	int appHeight = 400;
 
 	public static void main(String[] args) {
 		PApplet.main("com.makerjeff.new_processing_app.HalloWerld");
 	}
 	
 	public void settings(){
-		size(800, 400);
+		size(appWidth, appHeight);
 	}
 	public void setup() {
 		smooth();
-		frameRate(5.0f);
-		background(255,255,255);
+		frameRate(refresh);
+		background(255,210,0);
 		
 		//print array to console
 		printArray(Serial.list());
 				
-		fill(50,50,50);
+		fill(0,0,0);
+		textSize(16);
 		textAlign(LEFT);
+		
+		stroke(255, 230,0);
 		
 		//print serial ports on screen
 		for(int i = 0; i < Serial.list().length; i++) {
 			text("[" + i + "] " + Serial.list()[i], 10, 20 * i + 20);
 		}
 		
+		//serial port related setup
+		String portName = Serial.list()[5];	// dev/tty.usbmodem1a1321
+		myPort = new Serial(this, portName, 9600);
+		
 
 	}
 	public void draw() {
 		//connect_the_dots();
+		//stroke(255,230,0);
+		strokeWeight(lineWidth);
+		strokeCap(SQUARE);
+		line(xPos, height, xPos, yPos);
 		
-
+		//if line reaches end
+		if (xPos >= width) {
+			xPos = 0;	//reset back to beginning
+			background(255,210,0); //clear frame
+			
+			stroke(round(random(0,255)), round(random(0,255)), round(random(0,255)));
+		}
+		else{
+			xPos = xPos + lineWidth;
+		}
 	}
 	
 	public void mousePressed() {
 		//background(255);
+	}
+	
+	//special serial event
+	public void serialEvent(Serial myPort) {
+		//grab a byte
+		int inByte = myPort.read();
+		//print the byte
+		println(inByte);
+		
+		yPos = height - inByte;	//determine line height
 	}
 	
 	//CUSTOM FUNCTIONS
